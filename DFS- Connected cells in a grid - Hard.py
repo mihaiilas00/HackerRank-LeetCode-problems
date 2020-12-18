602a7977c6c77aebc52f2ab1741f4393647632a6
mihaiilas00 <60034906+mihaiilas00@users.noreply.github.com>
2020-12-18 17:03:19 +0200

    Update DFS- Connected cells in a grid - Hard.py

--- DFS- Connected cells in a grid - Hard.py
+++ DFS- Connected cells in a grid - Hard.py
@@ -1,7 +1,6 @@
 ####################################################################################################################
 #Consider a matrix where each cell contains either a  or a  and any cell containing a  is called a filled cell.
 #Two cells are said to be connected if they are adjacent to each other horizontally, vertically, or diagonally.
-#In the diagram below, the two colored regions show cells connected to the filled cells. Black on white are not connected.
 #
 #If one or more filled cells are also connected, they form a region.
 #Note that each cell in a region is connected to at least one other cell in the region but is not necessarily directly connected to all the other cells in the region.
